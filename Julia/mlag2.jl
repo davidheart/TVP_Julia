20204576ea6a4cd351048c671dd95a820546b235
Sung-guan Yun <davidheart@gmail.com>
2015-08-10 18:19:02 +0200

    Z matrix creation

--- Julia/mlag2.jl
+++ Julia/mlag2.jl
@@ -5,12 +5,12 @@ function mlag2(X,p)
   # MLAG2 Summary of this function goes here
   # Detailed explanation goes here
   Traw=size(X,1)
-  N=size(x,2)
+  N=size(X,2)
   Xlag=zeros(Traw,N*p);
   for ii in [1:p]
     Xlag[p+1:Traw,(N*(ii-1)+1):N*ii]=X[p+1-ii:Traw-ii,1:N]
-    return Xlag
   end
+  return Xlag
 end
 
 # #OR:
